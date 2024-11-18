e2fae1fb54d0fbd591cb5bdd7ab1d98cc55b7d8c
Pusho.Dev <bethocubans1990@gmail.com>
2024-11-18 14:47:55 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gestoremesas',
+  webDir: 'dist'
+};
+
+export default config;
