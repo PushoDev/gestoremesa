2c375e3804181d8b52d1105b6b18953e89080e53
Pusho.Dev <bethocubans1990@gmail.com>
2024-11-20 15:27:43 +0100

    Form casi terminado

--- src/components/home/AddClients.tsx
+++ src/components/home/AddClients.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import {
   IonButton,
   IonCard,
@@ -15,42 +15,57 @@ import {
 } from "@ionic/react";
 
 const AddClients = () => {
-  // Conectar Api Rest
-  fetch("https://sheetdb.io/api/v1/jom1wk8v9e84y", {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      data: [
-        {
-          id: "INCREMENT",
-          name_client: "Willian Guisado",
-          cell_client: "55572430",
-          envia_client: "$100,00",
-          name_familiar: "Laritza Cuñada",
-          cell_familiar: "56142247",
-          address_familiar: "Veguitas, Yara",
-          transaccion: "Efectivo",
-          moneda: "CUP",
-          card: "",
-          recive_familiar: "$32.000,00",
-          name_mensajero: "Luis Albero Guisado",
-          cell_mensajero: "50124578"
-        }
-      ]
-    })
-  })
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+  // Estado para almacenar los valores ingresados por el usuario
+  const [nombreEnvia, setNombreEnvia] = useState("");
+  const [telefonoEnvia, setTelefonoEnvia] = useState("");
+  const [cantidadEnvia, setCantidadEnvia] = useState("");
+  const [nombreFamiliar, setNombreFamiliar] = useState("");
+  const [telefonoFamiliar, setTelefonoFamiliar] = useState("");
+  const [direccionFamiliar, setDireccionFamiliar] = useState("");
+  const [transaccion, setTransaccion] = useState("");
+  const [moneda, setMoneda] = useState("");
+  const [cantidadRecive, setCantidadRecive] = useState("");
+  const [tarjetaFamiliar, setTarjetaFamiliar] = useState("");
+  const [nombreMensajero, setNombreMensajero] = useState("");
+  const [telefonoMensajero, setTelefonoMensajero] = useState("");
 
-  // Formato de la Tarjeta
-  const formatTarjeta = (tarjeta: string): string => {
-    const tarjetaFormateada = tarjeta
-      .replace(/\D/g, "")
-      .replace(/(\d{4})/g, "$1-");
-    return tarjetaFormateada;
+  // Función para enviar los valores a la API de SheetDB
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("https://sheetdb.io/api/v1/jom1wk8v9e84y", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          data: [
+            {
+              id: "INCREMENT",
+              name_client: nombreEnvia,
+              cell_client: telefonoEnvia,
+              envia_client: cantidadEnvia,
+              name_familiar: nombreFamiliar,
+              cell_familiar: telefonoFamiliar,
+              address_familiar: direccionFamiliar,
+              transaccion: transaccion,
+              moneda: moneda,
+              card: tarjetaFamiliar,
+              recive_familiar: cantidadRecive,
+              name_mensajero: nombreMensajero,
+              cell_mensajero: telefonoMensajero
+            }
+          ]
+        })
+      });
+
+      const createdRows = await response.json();
+      console.log(`Se crearon ${createdRows} filas`);
+    } catch (error) {
+      console.error("Error al crear filas:", error);
+    }
   };
 
   return (
@@ -68,6 +83,8 @@ const AddClients = () => {
             label="Nombre y Apellidos"
             labelPlacement="floating"
             placeholder="Credenciales de quien Envía"
+            value={nombreEnvia}
+            onIonChange={(e) => setNombreEnvia(e.detail.value!)}
           >
             {/* <IonIcon></IonIcon> */}
           </IonInput>
@@ -77,6 +94,8 @@ const AddClients = () => {
             label="Número de Teléfono"
             labelPlacement="floating"
             placeholder="Teléfono de Quien Envía"
+            value={telefonoEnvia}
+            onIonChange={(e) => setTelefonoEnvia(e.detail.value!)}
           >
             {/* <IonIcon></IonIcon> */}
           </IonInput>
@@ -86,6 +105,8 @@ const AddClients = () => {
             label="Cantidad que Envía"
             labelPlacement="floating"
             placeholder="Cantidad que Envía"
+            value={cantidadEnvia}
+            onIonChange={(e) => setCantidadEnvia(e.detail.value!)}
           >
             {/* <IonIcon></IonIcon> */}
           </IonInput>
@@ -100,6 +121,8 @@ const AddClients = () => {
             label="Número para Contactar"
             labelPlacement="floating"
             placeholder="Contacto de quien Recive"
+            value={telefonoFamiliar}
+            onIonChange={(e) => setTelefonoFamiliar(e.detail.value!)}
           >
             {/* <IonIcon></IonIcon> */}
           </IonInput>
@@ -109,13 +132,29 @@ const AddClients = () => {
             label="Nombre y Apellidos del Familiar"
             labelPlacement="floating"
             placeholder="Credenciales de quien Recive"
+            value={nombreFamiliar}
+            onIonChange={(e) => setNombreFamiliar(e.detail.value!)}
+          >
+            {/* <IonIcon></IonIcon> */}
+          </IonInput>
+          <br />
+          {/* Direccion de Residencia del Familiar */}
+          <IonInput
+            label="Dirección Particular"
+            labelPlacement="floating"
+            placeholder="Lugar donde vive el familiar o amigo"
+            value={nombreFamiliar}
+            onIonChange={(e) => setDireccionFamiliar(e.detail.value!)}
           >
             {/* <IonIcon></IonIcon> */}
           </IonInput>
           <br />
           {/* Accione para el Familiar */}
           <IonItem>
-            <IonSelect>
+            <IonSelect
+              value={transaccion}
+              onIonChange={(e) => setTransaccion(e.detail.value!)}
+            >
               <div slot="label">
                 <IonText color={"success"}>Tipo de Transacción</IonText>
               </div>
@@ -127,7 +166,10 @@ const AddClients = () => {
           {/* En caso de Escoger */}
           {/* Opción Efectivo */}
           <IonItem>
-            <IonSelect>
+            <IonSelect
+              value={moneda}
+              onIonChange={(e) => setMoneda(e.detail.value!)}
+            >
               <div color="warning">
                 <IonText>Tipo de Moneda</IonText>
               </div>
@@ -141,10 +183,15 @@ const AddClients = () => {
             labelPlacement="floating"
             label="Cantidad que Recive"
             placeholder="Monto que Recive"
+            value={cantidadRecive}
+            onIonChange={(e) => setCantidadRecive(e.detail.value!)}
           ></IonInput>
           {/* Opción Transferencia */}
           <IonItem>
-            <IonSelect>
+            <IonSelect
+              value={moneda}
+              onIonChange={(e) => setMoneda(e.detail.value!)}
+            >
               <div color="warning">
                 <IonText>Tipo de Moneda</IonText>
               </div>
@@ -158,6 +205,8 @@ const AddClients = () => {
             labelPlacement="floating"
             label="Número de Tarjeta"
             placeholder="Ingrese Número de Tarjeta"
+            value={tarjetaFamiliar}
+            onIonChange={(e) => setTarjetaFamiliar(e.detail.value!)}
           ></IonInput>
           <br />
           {/* Monto que Recive */}
@@ -165,6 +214,8 @@ const AddClients = () => {
             labelPlacement="floating"
             label="Cantidad que Recive"
             placeholder="Monto que Recive"
+            value={cantidadRecive}
+            onIonChange={(e) => setCantidadRecive(e.detail.value!)}
           ></IonInput>
           <br />
           <hr />
@@ -177,17 +228,21 @@ const AddClients = () => {
             labelPlacement="floating"
             label="Contacto del Mensajero"
             placeholder="Teléfono del Mensajero a Localizar"
+            value={telefonoMensajero}
+            onIonChange={(e) => setTelefonoMensajero(e.detail.value!)}
           ></IonInput>
           <br />
           <IonInput
             labelPlacement="floating"
             label="Nombre del Mensajero"
             placeholder="Mensajero que Entrega"
+            value={nombreMensajero}
+            onIonChange={(e) => setNombreMensajero(e.detail.value!)}
           ></IonInput>
           <br />
         </IonCardContent>
 
-        <IonButton fill="clear" color={"success"}>
+        <IonButton fill="clear" color={"success"} onClick={handleSubmit}>
           Agregar
         </IonButton>
         <IonButton fill="clear" color={"danger"}>
